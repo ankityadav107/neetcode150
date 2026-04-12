e8828090f31249550fc802209c06c974f9d719e5
Ankit Yadav <ankitrao380@gmail.com>
2026-04-12 13:25:58 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+         if (s.length !== t.length) return false;
+
+    let count = new Map();
+
+    for (let char of s) {
+        count.set(char, (count.get(char) || 0) + 1);
+    }
+
+    for (let char of t) {
+        if (!count.get(char)) return false;
+        count.set(char, count.get(char) - 1);
+    }
+
+    return true;
+}}
