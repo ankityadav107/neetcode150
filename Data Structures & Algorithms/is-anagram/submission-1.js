1096b1063d5f187ef0ddded81a5e336db6a87fec
Ankit Yadav <ankitrao380@gmail.com>
2026-04-12 13:22:50 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(s.length !== t.length) return false;
+        let map = new Map()
+
+        for(let ch of s){
+            map.set(ch,(map.get(ch) || 0) + 1 )
+        }
+        for(let ch of  t){
+            if(!map.has(ch)) return false
+
+            map.set(ch, map.get(ch) -1)
+            if(map.get(ch)<0) return false
+        }
+        return true
+    }
+}
